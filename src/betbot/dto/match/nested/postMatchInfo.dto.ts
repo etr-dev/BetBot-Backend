9573accffdab2e3cfc5c10c506c73a2c38c32945
erobin27 <elijah1998robinson@gmail.com>
2022-08-27 00:44:29 -0400

    edit num

--- src/betbot/dto/match/nested/postMatchInfo.dto.ts
+++ src/betbot/dto/match/nested/postMatchInfo.dto.ts
@@ -1,28 +1,35 @@
-import { IsOptional, IsString, ValidateNested } from "class-validator";
-import { CornerDetailsDto } from "./cornerDetails.dto";
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  ValidateNested,
+} from 'class-validator';
+import { CornerDetailsDto } from './cornerDetails.dto';
 
 export class PostMatchInfoDto {
-    @IsString()
-    @IsOptional()
-    result: string;
-  
-    @IsString()
-    @IsOptional()
-    method: string;
-  
-    @IsString()
-    @IsOptional()
-    time: string;
-  
-    @IsString()
-    @IsOptional()
-    round: number;
-  
-    @ValidateNested()
-    @IsOptional()
-    Red: CornerDetailsDto;
-  
-    @ValidateNested()
-    @IsOptional()
-    Blue: CornerDetailsDto;
-  }
+  @IsString()
+  @IsOptional()
+  result: string;
+
+  @IsString()
+  @IsOptional()
+  method: string;
+
+  @IsString()
+  @IsOptional()
+  time: string;
+
+  @IsNumber()
+  @Max(5)
+  @IsOptional()
+  round: number;
+
+  @ValidateNested()
+  @IsOptional()
+  Red: CornerDetailsDto;
+
+  @ValidateNested()
+  @IsOptional()
+  Blue: CornerDetailsDto;
+}

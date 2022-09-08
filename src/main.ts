5dacece5ac7e657684108479ec166cffe7cfb1e7
erobin27 <elijah1998robinson@gmail.com>
2022-09-08 15:07:39 -0400

    update port heroku

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT);
 
-  logServer(`BetBot database is running on port ${process.env.PORT || 3000}`)
+  logServer(`BetBot database is running on port ${process.env.PORT}`)
 }
 bootstrap();
